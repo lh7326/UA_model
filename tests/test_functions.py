bad0446107d75a0be68b25e0b0a9badc54fe188d
Lukas Holka <lukas.holka@gmail.com>
2022-03-03 20:38:19 +0100

    Implement a branch of the square root

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,18 +2,21 @@ import cmath
 from unittest import TestCase
 import math
 
-from ua_model.functions import z_minus_its_reciprocal
+from ua_model.functions import z_minus_its_reciprocal, square_root
 
 
 class TestFunctions(TestCase):
 
     def test_z_minus_its_reciprocal(self):
+
+        sqrt_2 = 1.4142135623730951
+
         test_cases = [
             {'argument': 1, 'expected_value': 0},
             {'argument': 1j, 'expected_value': 2j},
             {'argument': -2j, 'expected_value': -2.5j},
             {'argument': 1, 'expected_value': 0},
-            {'argument': (1 + 1j) / math.sqrt(2), 'expected_value': math.sqrt(2) * 1j},
+            {'argument': (1 + 1j) / sqrt_2, 'expected_value': sqrt_2 * 1j},
         ]
 
         for case in test_cases:
@@ -32,3 +35,26 @@ class TestFunctions(TestCase):
                 actual = z_minus_its_reciprocal(-1/z)
                 expected = z_minus_its_reciprocal(z)
                 self.assertTrue(cmath.isclose(actual, expected))
+
+    def test_square_root(self):
+        """Test our 'custom' branch of the square root"""
+
+        sqrt_2 = 1.4142135623730951
+        sqrt_534 = 23.108440016582687
+
+        test_cases = [
+            {'argument': 1, 'expected_value': 1},
+            {'argument': -1, 'expected_value': 1j},
+            {'argument': 1j, 'expected_value': (1 + 1j) / sqrt_2},
+            {'argument': -1j, 'expected_value': (-1 + 1j) / sqrt_2},
+            {'argument': 4, 'expected_value': 2},
+            {'argument': 534, 'expected_value': sqrt_534},
+            {'argument': -3 - 4j, 'expected_value': -1 + 2j},
+            {'argument': 10000 - 0.000000001j, 'expected_value': -100},
+            {'argument': -9j, 'expected_value': (-3 + 3j) / sqrt_2},
+        ]
+
+        for case in test_cases:
+            with self.subTest(case=case):
+                actual = square_root(case['argument'])
+                self.assertTrue(cmath.isclose(actual, case['expected_value']))
--- ua_model/functions.py
+++ ua_model/functions.py
@@ -3,6 +3,7 @@ This module contains some functions that will be useful in the construction of t
 
 """
 import cmath
+import math
 
 from ua_model.function_utils import compose
 
@@ -24,3 +25,25 @@ def z_minus_its_reciprocal(z: complex) -> complex:
 
     """
     return z - (1 / z)
+
+
+def square_root(z: complex) -> complex:
+    """
+    A branch of the square root. The cut is on the positive real axis; continuous from above.
+
+    We need a branch of the square root other than the principal one. We choose the branch analytic
+    on the complex plane minus the positive real numbers. I.e. z = r * exp(i * phi), r > 0, 0 <= phi < 2pi.
+    Then square_root(r * exp(i*phi))= sqrt(r) * exp(i*phi/2), with 0 <= phi/2 < pi.
+
+    Args:
+        z (complex):
+
+    Returns:
+        complex: A square root of z.
+
+    """
+    r, phi = cmath.polar(z)
+    # the original phi is from [-pi, pi]; but we do not want the branch cut on the negative real axis
+    if phi < 0:
+        phi = 2 * cmath.pi + phi
+    return math.sqrt(r) * cmath.exp(1j * phi / 2.0)
