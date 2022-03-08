7d5379dfd1954a4b20d60ce1be7108688f28f9e7
Lukas Holka <lukas.holka@gmail.com>
2022-03-08 18:52:24 +0100

    Add other basic plots

--- plotting/plot_complex.py
+++ plotting/plot_complex.py
@@ -6,6 +6,7 @@ import matplotlib.colors
 
 from ua_model.functions import square_root, z_minus_its_reciprocal
 from ua_model.MapFromWtoT import MapFromWtoT
+from ua_model.MapFromTtoW import MapFromTtoW
 
 
 def plot_mapped_curves(domain_title, range_title, domain_curves, mapped_curves):
@@ -61,15 +62,14 @@ def plot_z_minus_its_reciprocal(number_of_angles=5):
     plot_mapped_curves('z', 'z - 1/z', curves, mapped)
 
 
-def plot_w_to_t(t_0, t_in, number_of_angles=3):
-    curves_inside = [
-        [base * n / 100 for n in range(50, 100)] for base in
-        [cmath.exp(2j * cmath.pi * k / number_of_angles) for k in range(1, number_of_angles + 1)]
+def plot_w_to_t(t_0, t_in):
+    bases = [
+        cmath.exp(1j * cmath.pi * 2 / 3),
+        cmath.exp(1j * cmath.pi * 5 / 6),
+        cmath.exp(1j * cmath.pi * 8 / 7),
     ]
-    curves_outside = [
-        [base * (n + 50) / 50 for n in range(1, 100)] for base in
-        [cmath.exp(2j * cmath.pi * k / number_of_angles) for k in range(number_of_angles)]
-    ]
+    curves_inside = [[base * n / 1000 for n in range(100, 990)] for base in bases]
+    curves_outside = [[base * (n + 100) / 100 for n in range(10, 1000)] for base in bases]
     curves = curves_inside + curves_outside
 
     f = MapFromWtoT(t_0, t_in)
@@ -78,6 +78,23 @@ def plot_w_to_t(t_0, t_in, number_of_angles=3):
     plot_mapped_curves('W-plane', 't-plane', curves, mapped)
 
 
-if __name__ == '__main__':
-    plot_z_minus_its_reciprocal()
+def plot_t_to_w(t_0, t_in):
+    curves = [
+        [(n * -0.1 + t_0) for n in range(1, 1000)],  # between minus infinity and t_0
+        [((t_in - t_0) * n * 0.01 + t_0) for n in range(1, 100)],  # between t_0 and t_in
+        [(n * 0.1 + t_in) for n in range(1, 1000)],  # above t_in
+        [(n * -0.1 + 54j) for n in range(1, 1000)],
+        [(n * 0.1 + 54j) for n in range(1, 1000)],
+        [(n * 0.1 - 20j) for n in range(-1000, 0)] + [(n * 0.1 - 20j) for n in range(1, 1000)],
+        [n * (0.1 + 0.1j) for n in range(1, 1000)],
+        [(n * 0.1j + 17) for n in range(1, 1000)],
+    ]
 
+    f = MapFromTtoW(t_0, t_in)
+    mapped = [[f(z) for z in curve] for curve in curves]
+
+    plot_mapped_curves('t-plane', 'W-plane', curves, mapped)
+
+
+if __name__ == '__main__':
+    plot_t_to_w(t_0=1, t_in=2)
