083921095128facf2220591942e96993295cfb7e
Lukas Holka <lukas.holka@gmail.com>
2022-03-29 16:41:07 +0200

    Separate ModelParameters into a module, add tests

---
+++ kaon_production/ModelParameters.py
@@ -0,0 +1,158 @@
+from collections import namedtuple
+import math
+import numpy as np
+
+
+Parameter = namedtuple('Parameter', 'name value is_fixed')
+
+
+class ModelParameters:
+
+    def __init__(self,
+                 t_0_isoscalar, t_0_isovector,
+                 t_in_isoscalar, t_in_isovector,
+                 a_omega, mass_omega, decay_rate_omega,
+                 a_omega_prime, mass_omega_prime, decay_rate_omega_prime,
+                 a_omega_double_prime, mass_omega_double_prime, decay_rate_omega_double_prime,
+                 a_phi, mass_phi, decay_rate_phi,
+                 a_phi_prime, mass_phi_prime, decay_rate_phi_prime,
+                 mass_phi_double_prime, decay_rate_phi_double_prime,
+                 a_rho, mass_rho, decay_rate_rho,
+                 a_rho_prime, mass_rho_prime, decay_rate_rho_prime,
+                 a_rho_double_prime, mass_rho_double_prime, decay_rate_rho_double_prime,
+                 mass_rho_triple_prime, decay_rate_rho_triple_prime):
+        self.t_0_isoscalar = t_0_isoscalar
+        self.t_0_isovector = t_0_isovector
+        self._data = [
+            Parameter(name='t_in_isoscalar', value=t_in_isoscalar, is_fixed=False),
+            Parameter(name='t_in_isovector', value=t_in_isovector, is_fixed=False),
+            Parameter(name='a_omega', value=a_omega, is_fixed=False),
+            Parameter(name='mass_omega', value=mass_omega, is_fixed=False),
+            Parameter(name='decay_rate_omega', value=decay_rate_omega, is_fixed=False),
+            Parameter(name='a_omega_prime', value=a_omega_prime, is_fixed=False),
+            Parameter(name='mass_omega_prime', value=mass_omega_prime, is_fixed=False),
+            Parameter(name='decay_rate_omega_prime', value=decay_rate_omega_prime, is_fixed=False),
+            Parameter(name='a_omega_double_prime', value=a_omega_double_prime, is_fixed=False),
+            Parameter(name='mass_omega_double_prime', value=mass_omega_double_prime, is_fixed=False),
+            Parameter(name='decay_rate_omega_double_prime', value=decay_rate_omega_double_prime, is_fixed=False),
+            Parameter(name='a_phi', value=a_phi, is_fixed=False),
+            Parameter(name='mass_phi', value=mass_phi, is_fixed=False),
+            Parameter(name='decay_rate_phi', value=decay_rate_phi, is_fixed=False),
+            Parameter(name='a_phi_prime', value=a_phi_prime, is_fixed=False),
+            Parameter(name='mass_phi_prime', value=mass_phi_prime, is_fixed=False),
+            Parameter(name='decay_rate_phi_prime', value=decay_rate_phi_prime, is_fixed=False),
+            Parameter(name='mass_phi_double_prime', value=mass_phi_double_prime, is_fixed=False),
+            Parameter(name='decay_rate_phi_double_prime', value=decay_rate_phi_double_prime, is_fixed=False),
+            Parameter(name='a_rho', value=a_rho, is_fixed=False),
+            Parameter(name='mass_rho', value=mass_rho, is_fixed=False),
+            Parameter(name='decay_rate_rho', value=decay_rate_rho, is_fixed=False),
+            Parameter(name='a_rho_prime', value=a_rho_prime, is_fixed=False),
+            Parameter(name='mass_rho_prime', value=mass_rho_prime, is_fixed=False),
+            Parameter(name='decay_rate_rho_prime', value=decay_rate_rho_prime, is_fixed=False),
+            Parameter(name='a_rho_double_prime', value=a_rho_double_prime, is_fixed=False),
+            Parameter(name='mass_rho_double_prime', value=mass_rho_double_prime, is_fixed=False),
+            Parameter(name='decay_rate_rho_double_prime', value=decay_rate_rho_double_prime, is_fixed=False),
+            Parameter(name='mass_rho_triple_prime', value=mass_rho_triple_prime, is_fixed=False),
+            Parameter(name='decay_rate_rho_triple_prime', value=decay_rate_rho_triple_prime, is_fixed=False),
+        ]
+
+    def _find(self, name):
+        for index, parameter in enumerate(self._data):
+            if parameter.name == name:
+                return index, parameter
+        raise KeyError(f'No such key: {name}')
+
+    def __getitem__(self, item):
+        _, parameter = self._find(item)
+        return parameter
+
+    def fix_parameters(self, names):
+        for name in names:
+            index, parameter = self._find(name)
+            self._data[index] = Parameter(parameter.name, parameter.value, True)
+
+    def release_parameters(self, names):
+        for name in names:
+            index, parameter = self._find(name)
+            self._data[index] = Parameter(parameter.name, parameter.value, False)
+
+    def release_all_parameters(self):
+        for index, parameter in enumerate(self._data):
+            self._data[index] = Parameter(parameter.name, parameter.value, False)
+
+    def get_fixed_values(self):
+        return [parameter.value for parameter in self._data if parameter.is_fixed]
+
+    def get_free_values(self):
+        return [parameter.value for parameter in self._data if not parameter.is_fixed]
+
+    def get_all_values(self):
+        return list(map(lambda p: p.value, self._data))
+
+    def update_free_values(self, new_values):
+        free_parameters = [(i, parameter) for i, parameter
+                           in enumerate(self._data) if not parameter.is_fixed]
+        if len(free_parameters) != len(new_values):
+            raise ValueError(f'Wrong number of new values: got {len(new_values)}, requires {len(free_parameters)}.')
+        for (i, parameter), new_value in zip(free_parameters, new_values):
+            self._data[i] = Parameter(parameter.name, new_value, parameter.is_fixed)
+
+    def __iter__(self):
+        for parameter in self._data:
+            yield parameter
+
+    def to_list(self):
+        return list(self._data)
+
+    def get_bounds_for_free_parameters(self):
+        full_bounds = self.get_model_parameters_bounds()
+        lower_bounds = []
+        upper_bounds = []
+        for parameter in self._data:
+            if not parameter.is_fixed:
+                bounds = full_bounds[parameter.name]
+                lower_bounds.append(bounds['lower'])
+                upper_bounds.append(bounds['upper'])
+        return lower_bounds, upper_bounds
+
+    def get_model_parameters_bounds(self):
+        """
+        There are no upper bounds. The branch points t_in_isoscalar and t_in_isovector
+        must lie above the values of t_0_isoscalar and t_0_isovector, respectively.
+        Decay rates must be non-negative and squared masses must lie above their respective t_0 treshold.
+
+        """
+        lower_mass_bound_isoscalar = math.sqrt(self.t_0_isoscalar)
+        lower_mass_bound_isovector = math.sqrt(self.t_0_isovector)
+        return {
+            't_in_isoscalar': {'lower': self.t_0_isoscalar, 'upper': np.inf},
+            't_in_isovector': {'lower': self.t_0_isovector, 'upper': np.inf},
+            'a_omega': {'lower': -np.inf, 'upper': np.inf},
+            'mass_omega': {'lower': lower_mass_bound_isoscalar, 'upper': np.inf},
+            'decay_rate_omega': {'lower': 0.0, 'upper': np.inf},
+            'a_omega_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_omega_prime': {'lower': lower_mass_bound_isoscalar, 'upper': np.inf},
+            'decay_rate_omega_prime': {'lower': 0.0, 'upper': np.inf},
+            'a_omega_double_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_omega_double_prime': {'lower': lower_mass_bound_isoscalar, 'upper': np.inf},
+            'decay_rate_omega_double_prime': {'lower': 0.0, 'upper': np.inf},
+            'a_phi': {'lower': -np.inf, 'upper': np.inf},
+            'mass_phi': {'lower': lower_mass_bound_isoscalar, 'upper': np.inf},
+            'decay_rate_phi': {'lower': 0.0, 'upper': np.inf},
+            'a_phi_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_phi_prime': {'lower': lower_mass_bound_isoscalar, 'upper': np.inf},
+            'decay_rate_phi_prime': {'lower': 0.0, 'upper': np.inf},
+            'mass_phi_double_prime': {'lower': lower_mass_bound_isoscalar, 'upper': np.inf},
+            'decay_rate_phi_double_prime': {'lower': 0.0, 'upper': np.inf},
+            'a_rho': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho': {'lower': lower_mass_bound_isovector, 'upper': np.inf},
+            'decay_rate_rho': {'lower': 0.0, 'upper': np.inf},
+            'a_rho_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho_prime': {'lower': lower_mass_bound_isovector, 'upper': np.inf},
+            'decay_rate_rho_prime': {'lower': 0.0, 'upper': np.inf},
+            'a_rho_double_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho_double_prime': {'lower': lower_mass_bound_isovector, 'upper': np.inf},
+            'decay_rate_rho_double_prime': {'lower': 0.0, 'upper': np.inf},
+            'mass_rho_triple_prime': {'lower': lower_mass_bound_isovector, 'upper': np.inf},
+            'decay_rate_rho_triple_prime': {'lower': 0.0, 'upper': np.inf},
+        }
---
+++ tests/test_ModelParameters.py
@@ -0,0 +1,144 @@
+from unittest import TestCase
+import numpy as np
+
+from kaon_production.ModelParameters import ModelParameters
+
+
+class TestModelParameters(TestCase):
+
+    def setUp(self):
+        self.parameters = ModelParameters(
+            0.5, 0.6, 0.7, 0.8,
+            0.1, 0.78266, 0.00868,
+            0.2, 1.410, 0.29,
+            0.15, 1.67, 0.315,
+            0.3, 1.019461, 0.004249,
+            0.35, 1.680, 0.150,
+            2.159, 0.137,
+            0.12, 0.77526, 0.1474,
+            0.13, 1.465, 0.4,
+            0.14, 1.720, 0.25,
+            2.15, 0.3,
+        )
+
+    def test___getitem__(self):
+        with self.subTest():
+            par1 = self.parameters['a_rho_double_prime']
+            self.assertEqual(par1.value, 0.14)
+        with self.subTest():
+            par2 = self.parameters['mass_phi_prime']
+            self.assertEqual(par2.value, 1.680)
+        with self.subTest():
+            par3 = self.parameters['t_in_isovector']
+            self.assertEqual(par3.value, 0.8)
+        with self.subTest():
+            with self.assertRaises(KeyError):
+                _ = self.parameters['non-existent-key']
+
+    def test_fix_parameters(self):
+        self.parameters.release_all_parameters()
+        self.parameters.fix_parameters(['mass_phi', 't_in_isoscalar'])
+        with self.subTest():
+            self.assertTrue(self.parameters['mass_phi'].is_fixed)
+        with self.subTest():
+            self.assertTrue(self.parameters['t_in_isoscalar'].is_fixed)
+
+    def test_release_parameters(self):
+        self.parameters.fix_parameters(['a_rho_double_prime'])
+        self.parameters.release_parameters(['a_rho_double_prime'])
+        self.assertFalse(self.parameters['a_rho_double_prime'].is_fixed)
+
+    def test_release_all_parameters(self):
+        self.parameters.fix_parameters(['a_phi', 'mass_omega_prime', 't_in_isovector'])
+        self.parameters.release_all_parameters()
+        self.assertFalse(self.parameters['a_phi'].is_fixed)
+        self.assertFalse(self.parameters['mass_omega_prime'].is_fixed)
+        self.assertFalse(self.parameters['t_in_isovector'].is_fixed)
+
+    def test_get_fixed_values(self):
+        self.parameters.release_all_parameters()
+        self.parameters.fix_parameters(['a_omega_double_prime', 'mass_rho_prime', 't_in_isovector'])
+        self.assertListEqual(
+            self.parameters.get_fixed_values(),
+            [0.8, 0.15, 1.465],
+        )
+
+    def test_get_free_values(self):
+        self.parameters.release_all_parameters()
+        self.parameters.fix_parameters([
+            'a_phi', 'mass_phi', 'decay_rate_phi',
+            'a_phi_prime', 'mass_phi_prime', 'decay_rate_phi_prime',
+            'mass_phi_double_prime', 'decay_rate_phi_double_prime',
+            'a_omega', 'mass_omega', 'decay_rate_omega',
+            'a_omega_prime', 'mass_omega_prime', 'decay_rate_omega_prime',
+            'a_omega_double_prime', 'mass_omega_double_prime', 'decay_rate_omega_double_prime',
+        ])
+        self.assertListEqual(
+            self.parameters.get_free_values(),
+            [0.7, 0.8, 0.12, 0.77526, 0.1474, 0.13, 1.465, 0.4,
+             0.14, 1.720, 0.25, 2.15, 0.3],
+        )
+
+    def test_get_all_values(self):
+        self.assertListEqual(
+            self.parameters.get_all_values(),
+            [0.7, 0.8, 0.1, 0.78266, 0.00868, 0.2, 1.410, 0.29,
+             0.15, 1.67, 0.315, 0.3, 1.019461, 0.004249, 0.35, 1.680, 0.150,
+             2.159, 0.137, 0.12, 0.77526, 0.1474, 0.13, 1.465, 0.4,
+             0.14, 1.720, 0.25, 2.15, 0.3],
+        )
+
+    def test_update_free_values(self):
+        self.parameters.release_all_parameters()
+        self.parameters.fix_parameters([
+            'a_phi', 'mass_phi', 'decay_rate_phi',
+            'a_phi_prime', 'mass_phi_prime', 'decay_rate_phi_prime',
+            'mass_phi_double_prime', 'decay_rate_phi_double_prime',
+            'a_omega', 'mass_omega', 'decay_rate_omega',
+            'a_omega_prime', 'mass_omega_prime', 'decay_rate_omega_prime',
+            'a_omega_double_prime', 'mass_omega_double_prime', 'decay_rate_omega_double_prime',
+        ])
+        self.parameters.update_free_values(
+            [0.7, 0.82, 0.12, 0.7, 0.051, 0.0, 14.65, 0.4,
+             0.24, 1.724, 0.25, 0.15, 7.4]
+        )
+        self.assertListEqual(
+            self.parameters.get_free_values(),
+            [0.7, 0.82, 0.12, 0.7, 0.051, 0.0, 14.65, 0.4,
+             0.24, 1.724, 0.25, 0.15, 7.4]
+        )
+
+    def test_update_free_values__length_mismatch(self):
+        self.parameters.release_all_parameters()
+        with self.assertRaises(ValueError):
+            self.parameters.update_free_values([1])
+
+    def test___iter__(self):
+        self.assertListEqual(
+            [parameter.value for parameter in self.parameters],
+            self.parameters.get_all_values(),
+        )
+
+    def test_get_bounds_for_free_parameters(self):
+        self.parameters.release_all_parameters()
+        self.parameters.fix_parameters([
+            't_in_isovector',
+            'a_phi', 'decay_rate_phi',
+            'a_phi_prime', 'mass_phi_prime', 'decay_rate_phi_prime',
+            'mass_phi_double_prime', 'decay_rate_phi_double_prime',
+            'a_omega', 'mass_omega', 'decay_rate_omega',
+            'a_omega_prime', 'mass_omega_prime', 'decay_rate_omega_prime',
+            'a_omega_double_prime', 'mass_omega_double_prime', 'decay_rate_omega_double_prime',
+            'a_rho', 'mass_rho', 'decay_rate_rho',
+            'a_rho_prime', 'mass_rho_prime', 'decay_rate_rho_prime',
+            'mass_rho_triple_prime', 'decay_rate_rho_triple_prime',
+        ])
+        lower_bounds, upper_bounds = self.parameters.get_bounds_for_free_parameters()
+        self.assertListEqual(
+            lower_bounds,
+            [0.5, 0.7071067811865476, -np.inf, 0.7745966692414834, 0.0],
+        )
+        self.assertListEqual(
+            upper_bounds,
+            [np.inf, np.inf, np.inf, np.inf, np.inf],
+        )
