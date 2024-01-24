dc907d7fc079988b7e6b25d8140b60fd26611323
Lukas Holka <lukas.holka@savba.sk>
2024-01-24 11:07:40 +0100

    Add a pion FF model

---
+++ model_parameters/PionParameters.py
@@ -0,0 +1,195 @@
+import math
+import numpy as np
+from typing import Dict, List, Tuple
+
+from model_parameters.ModelParameters import Parameter, ModelParameters
+
+
+class PionParameters(ModelParameters):
+
+    def __init__(self,
+                 t_0_isovector: float,
+                 t_in_isovector: float,
+                 a_rho: float,
+                 mass_rho: float,
+                 decay_rate_rho: float,
+                 a_rho_prime: float,
+                 mass_rho_prime: float,
+                 decay_rate_rho_prime: float,
+                 a_rho_double_prime: float,
+                 mass_rho_double_prime: float,
+                 decay_rate_rho_double_prime: float,
+                 mass_rho_triple_prime: float,
+                 decay_rate_rho_triple_prime: float) -> None:
+
+        super().__init__(
+            t_0_isovector, t_in_isovector,
+            a_rho, mass_rho, decay_rate_rho,
+            a_rho_prime, mass_rho_prime, decay_rate_rho_prime,
+            a_rho_double_prime, mass_rho_double_prime, decay_rate_rho_double_prime,
+            mass_rho_triple_prime, decay_rate_rho_triple_prime,
+            always_fixed=('t_0_isovector',),
+        )
+
+    def _setup_data(self,
+                    t_0_isovector: float,
+                    t_in_isovector: float,
+                    a_rho: float,
+                    mass_rho: float,
+                    decay_rate_rho: float,
+                    a_rho_prime: float,
+                    mass_rho_prime: float,
+                    decay_rate_rho_prime: float,
+                    a_rho_double_prime: float,
+                    mass_rho_double_prime: float,
+                    decay_rate_rho_double_prime: float,
+                    mass_rho_triple_prime: float,
+                    decay_rate_rho_triple_prime: float) -> List[Parameter]:
+
+        return [
+            Parameter(name='t_0_isovector', value=t_0_isovector, is_fixed=True),
+            Parameter(name='t_in_isovector', value=t_in_isovector, is_fixed=False),
+            Parameter(name='a_rho', value=a_rho, is_fixed=False),
+            Parameter(name='mass_rho', value=mass_rho, is_fixed=False),
+            Parameter(name='decay_rate_rho', value=decay_rate_rho, is_fixed=False),
+            Parameter(name='a_rho_prime', value=a_rho_prime, is_fixed=False),
+            Parameter(name='mass_rho_prime', value=mass_rho_prime, is_fixed=False),
+            Parameter(name='decay_rate_rho_prime', value=decay_rate_rho_prime, is_fixed=False),
+            Parameter(name='a_rho_double_prime', value=a_rho_double_prime, is_fixed=False),
+            Parameter(name='mass_rho_double_prime', value=mass_rho_double_prime, is_fixed=False),
+            Parameter(name='decay_rate_rho_double_prime', value=decay_rate_rho_double_prime, is_fixed=False),
+            Parameter(name='mass_rho_triple_prime', value=mass_rho_triple_prime, is_fixed=False),
+            Parameter(name='decay_rate_rho_triple_prime', value=decay_rate_rho_triple_prime, is_fixed=False),
+        ]
+
+    @classmethod
+    def from_list(cls, list_of_parameters: List[Parameter]) -> 'PionParameters':
+        kwargs = {par.name: par.value for par in list_of_parameters}
+        instance = cls(**kwargs)
+        parameters_to_fix = [p.name for p in list_of_parameters if p.is_fixed]
+        instance.release_all_parameters()
+        instance.fix_parameters(parameters_to_fix)
+        return instance
+
+    def get_bounds_for_free_parameters(self, handpicked: bool = True) -> Tuple[List[float], List[float]]:
+        if handpicked:
+            full_bounds = self.get_model_parameters_bounds_handpicked()
+        else:
+            full_bounds = self.get_model_parameters_bounds_maximal()
+        lower_bounds = []
+        upper_bounds = []
+        for parameter in self._data:
+            if not parameter.is_fixed:
+                bounds = full_bounds[parameter.name]
+                lower_bounds.append(bounds['lower'])
+                upper_bounds.append(bounds['upper'])
+        return lower_bounds, upper_bounds
+
+    def get_model_parameters_bounds_handpicked(self) -> Dict:
+        """
+        Returns a handpicked set of bounds.
+
+        """
+        return {
+            't_in_isovector': {'lower': self['t_0_isovector'].value, 'upper': np.inf},
+            'a_rho': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho': {'lower': 0.75, 'upper': 0.78},
+            'decay_rate_rho': {'lower': 0.140, 'upper': 0.150},
+            'a_rho_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho_prime': {'lower': 1.28, 'upper': 1.48},
+            'decay_rate_rho_prime': {'lower': 0.15, 'upper': 0.55},
+            'a_rho_double_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho_double_prime': {'lower': 1.68, 'upper': 1.74},
+            'decay_rate_rho_double_prime': {'lower': 0.1, 'upper': 0.6},
+            'mass_rho_triple_prime': {'lower': 2.0, 'upper': 2.4},
+            'decay_rate_rho_triple_prime': {'lower': 0.1, 'upper': 0.6},
+        }
+
+    def get_model_parameters_bounds_maximal(self) -> Dict:
+        """
+        There are no upper bounds. The branch points t_in_isoscalar and t_in_isovector
+        must lie above the values of t_0_isoscalar and t_0_isovector, respectively.
+        Decay rates must be non-negative and squared masses must lie above their respective t_0 treshold.
+
+        """
+        lower_mass_bound_isovector = math.sqrt(self['t_0_isovector'].value)
+        return {
+            't_in_isovector': {'lower': self['t_0_isovector'].value, 'upper': np.inf},
+            'a_rho': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho': {'lower': lower_mass_bound_isovector, 'upper': np.inf},
+            'decay_rate_rho': {'lower': 0.0, 'upper': np.inf},
+            'a_rho_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho_prime': {'lower': lower_mass_bound_isovector, 'upper': np.inf},
+            'decay_rate_rho_prime': {'lower': 0.0, 'upper': np.inf},
+            'a_rho_double_prime': {'lower': -np.inf, 'upper': np.inf},
+            'mass_rho_double_prime': {'lower': lower_mass_bound_isovector, 'upper': np.inf},
+            'decay_rate_rho_double_prime': {'lower': 0.0, 'upper': np.inf},
+            'mass_rho_triple_prime': {'lower': lower_mass_bound_isovector, 'upper': np.inf},
+            'decay_rate_rho_triple_prime': {'lower': 0.0, 'upper': np.inf},
+        }
+
+    def get_ordered_values(self) -> List[float]:
+        """
+        Print the values in the order:
+          t_in_isovector
+          mass_rho
+          mass_rho_prime
+          mass_rho_double_prime
+          mass_rho_triple_prime
+          decay_rate_rho
+          decay_rate_rho_prime
+          decay_rate_rho_double_prime
+          decay_rate_rho_triple_prime
+          a_rho
+          a_rho_prime
+          a_rho_double_prime
+
+        Returns: the ordered list of parameter values
+
+        """
+        names = [
+            't_in_isovector',
+            'mass_rho',
+            'mass_rho_prime',
+            'mass_rho_double_prime',
+            'mass_rho_triple_prime',
+            'decay_rate_rho',
+            'decay_rate_rho_prime',
+            'decay_rate_rho_double_prime',
+            'decay_rate_rho_triple_prime',
+            'a_rho',
+            'a_rho_prime',
+            'a_rho_double_prime',
+        ]
+        return [self[name].value for name in names]
+
+    @classmethod
+    def from_ordered_values(
+            cls,
+            list_of_values: List[float],
+            t_0_isovector: float) -> 'PionParameters':
+        names = [
+            't_in_isovector',
+            'mass_rho',
+            'mass_rho_prime',
+            'mass_rho_double_prime',
+            'mass_rho_triple_prime',
+            'decay_rate_rho',
+            'decay_rate_rho_prime',
+            'decay_rate_rho_double_prime',
+            'decay_rate_rho_triple_prime',
+            'a_rho',
+            'a_rho_prime',
+            'a_rho_double_prime',
+        ]
+        assert len(list_of_values) == len(names)
+        pars = [Parameter(name, value, False) for name, value in zip(names, list_of_values)]
+        pars.extend([
+            Parameter('t_0_isovector', t_0_isovector, True),
+        ])
+        return cls.from_list(pars)
+
+    def fix_resonances(self):
+        self.fix_parameters(['mass_rho', 'mass_rho_prime', 'mass_rho_double_prime', 'mass_rho_triple_prime',
+                             'decay_rate_rho', 'decay_rate_rho_prime', 'decay_rate_rho_double_prime',
+                             'decay_rate_rho_triple_prime'])
---
+++ ua_model/PionUAModel.py
@@ -0,0 +1,90 @@
+from ua_model.ua_components.UAComponent import UAComponent
+from ua_model.ua_components.UAComponentVariantA import UAComponentVariantA
+from ua_model.ua_components.UAComponentVariantB import UAComponentVariantB
+from ua_model.MapFromTtoW import MapFromTtoW
+
+
+class PionUAModel:
+    """
+    The U&A model for the charged pion form factor.
+
+    """
+    def __init__(
+            self,
+            t_0_isovector: float,
+            t_in_isovector: float,
+            a_rho: float,
+            a_rho_prime: float,
+            a_rho_double_prime: float,
+            mass_rho: float,
+            decay_rate_rho: float,
+            mass_rho_prime: float,
+            decay_rate_rho_prime: float,
+            mass_rho_double_prime: float,
+            decay_rate_rho_double_prime: float,
+            mass_rho_triple_prime: float,
+            decay_rate_rho_triple_prime: float,
+    ) -> None:
+        self.t_0_isovector = t_0_isovector
+        self.t_in_isovector = t_in_isovector
+
+        # isovector components' proportionality constants
+        self.a_rho = a_rho
+        self.a_rho_prime = a_rho_prime
+        self.a_rho_double_prime = a_rho_double_prime
+        self.a_rho_triple_prime = 0.5 - a_rho - a_rho_prime - a_rho_double_prime
+
+        # read data about the resonances
+        self.mass_rho = mass_rho
+        self.decay_rate_rho = decay_rate_rho
+        self.mass_rho_prime = mass_rho_prime
+        self.decay_rate_rho_prime = decay_rate_rho_prime
+        self.mass_rho_double_prime = mass_rho_double_prime
+        self.decay_rate_rho_double_prime = decay_rate_rho_double_prime
+        self.mass_rho_triple_prime = mass_rho_triple_prime
+        self.decay_rate_rho_triple_prime = decay_rate_rho_triple_prime
+
+        self._t_to_W_isovector = MapFromTtoW(
+            t_0=self.t_0_isovector,
+            t_in=self.t_in_isovector,
+        )
+
+        self._component_rho = None
+        self._component_rho_prime = None
+        self._component_rho_double_prime = None
+        self._component_rho_triple_prime = None
+        self._initialize_isovector_components()
+
+    def __call__(self, t: complex) -> complex:
+        return self._eval_isovector_contribution(t)
+
+    def _eval_isovector_contribution(self, t: complex) -> complex:
+        w = self._t_to_W_isovector(t)
+        return (
+            self.a_rho * self._component_rho(w)
+            + self.a_rho_prime * self._component_rho_prime(w)
+            + self.a_rho_double_prime * self._component_rho_double_prime(w)
+            + self.a_rho_triple_prime * self._component_rho_triple_prime(w)
+        )
+
+    def _initialize_isovector_components(self) -> None:
+        for resonance_name in ['rho', 'rho_prime', 'rho_double_prime', 'rho_triple_prime']:
+            mass = self.__getattribute__('mass_' + resonance_name)
+            decay_rate = self.__getattribute__('decay_rate_' + resonance_name)
+            component = self._build_component(self.t_0_isovector, self.t_in_isovector, mass, decay_rate)
+            self.__setattr__('_component_' + resonance_name, component)
+
+    @staticmethod
+    def _build_component(t_0: float, t_in: float, mass: float, decay_rate: float) -> UAComponent:
+        map_from_t_to_w = MapFromTtoW(t_0, t_in)
+        w_n = map_from_t_to_w(0)
+        t_meson_pole = (mass - 1j * decay_rate / 2) ** 2
+        w_meson = map_from_t_to_w(t_meson_pole)
+
+        mass_squared = mass**2
+        if mass_squared < t_0:
+            raise ValueError('Mass squared of the resonance must be above the t_0 threshold!')
+        elif mass_squared < t_in:
+            return UAComponentVariantA(w_n, w_meson)
+        else:
+            return UAComponentVariantB(w_n, w_meson)
