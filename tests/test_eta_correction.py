e8acc9ac805983424792016ebf9d928cda5d2f43
Lukas Holka <lukas.holka@savba.sk>
2024-03-11 11:33:46 +0100

    Add tests

--- tests/test_eta_correction.py
+++ tests/test_eta_correction.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 import math
 
-from kaon_production.eta_correction import calculate_beta, dilogarithm, add_fsr_effects
-# TODO: finish
+from kaon_production.eta_correction import (
+    calculate_beta, dilogarithm, add_fsr_effects, calculate_sommerfeld_gamow_sakharov_factor,
+    calculate_cremmer_gourdin_factor,)
+
 
 class TestEtaCorrection(TestCase):
 
@@ -54,4 +56,55 @@ class TestEtaCorrection(TestCase):
         for case in test_cases:
             with self.subTest(msg=f'case={case}'):
                 actual = add_fsr_effects(case['cs'], case['s'], particle_mass, alpha)
+                self.assertTrue(math.isclose(actual, case['expected'], abs_tol=1e-15))
+
+    def test_calculate_sommerfeld_gamow_sakharov_factor(self):
+        alpha = 0.0072973525693
+        charged_kaon_mass = 0.493677
+
+        test_cases = [
+            {'s': 0.992, 'expected': 1.0905987714012446},
+            {'s': 1.010, 'expected': 1.0631257323094139},
+            {'s': 1.038, 'expected': 1.047428440327931},
+            {'s': 1.041, 'expected': 1.0463868938251664},
+            {'s': 1.921, 'expected': 1.01644966238823},
+        ]
+
+        for case in test_cases:
+            with self.subTest(msg=f'case={case}'):
+                actual = calculate_sommerfeld_gamow_sakharov_factor(case['s'], alpha, charged_kaon_mass)
+                self.assertTrue(math.isclose(actual, case['expected'], abs_tol=1e-15))
+
+    def test_calculate_cremmer_gourdin_factor(self):
+        alpha = 0.0072973525693
+        charged_kaon_mass = 0.493677
+
+        test_cases = [
+            {'s': 0.992, 'expected': 1.0833921887470144},
+            {'s': 1.010, 'expected': 1.0575466529534336},
+            {'s': 1.038, 'expected': 1.0421733717001378},
+            {'s': 1.041, 'expected': 1.041120787219781},
+            {'s': 1.921, 'expected': 0.9936804504679941},
+        ]
+
+        for case in test_cases:
+            with self.subTest(msg=f'case={case}'):
+                actual = calculate_cremmer_gourdin_factor(case['s'], alpha, charged_kaon_mass)
+                self.assertTrue(math.isclose(actual, case['expected'], abs_tol=1e-15))
+
+    def test_add_fsr_effects__2(self):
+        alpha = 0.0072973525693
+        charged_kaon_mass = 0.493677
+
+        test_cases = [
+            {'s': 0.992, 'expected': 1.0835076399647567},
+            {'s': 1.010, 'expected': 1.0579358984255511},
+            {'s': 1.038, 'expected': 1.043082719130355},
+            {'s': 1.041, 'expected': 1.042089879597166},
+            {'s': 1.921, 'expected': 1.0125361880178845},
+        ]
+
+        for case in test_cases:
+            with self.subTest(msg=f'case={case}'):
+                actual = add_fsr_effects(1.0, case['s'], charged_kaon_mass, alpha)
                 self.assertTrue(math.isclose(actual, case['expected'], abs_tol=1e-15))
