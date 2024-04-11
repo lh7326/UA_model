cf059cf7854a8fe1e5a76ded42c5d3a584239449
Lukas Holka <lukas.holka@savba.sk>
2024-04-11 11:10:47 +0200

    Update the R ratio script

--- kaon_production/calculate_r_ratio.py
+++ kaon_production/calculate_r_ratio.py
@@ -1,9 +1,10 @@
 from configparser import ConfigParser
 import math
-from typing import Tuple
-from model_parameters import KaonParametersPhiRatio
-
+from typing import Tuple, Union
+from model_parameters import KaonParametersPhiRatio, KaonParametersPhiRatioSimple
 from kaon_production.eta_correction import calculate_cremmer_gourdin_factor
+from common.utils import make_partial_ff_or_cs_for_parameters
+from kaon_production.data import KaonDatapoint
 
 
 def _calculate_kinematic_factor(phi_mass: float, charged_kaon_mass: float, neutral_kaon_mass: float) -> float:
@@ -27,7 +28,7 @@ def _calculate_kinematic_factor_with_errors(
 
 
 def calculate_r_ratio(
-        parameters: KaonParametersPhiRatio,
+        parameters: Union[KaonParametersPhiRatio, KaonParametersPhiRatioSimple],
         charged_kaon_mass: float,
         neutral_kaon_mass: float,
         alpha: float,
@@ -43,8 +44,36 @@ def calculate_r_ratio(
     return radiative_correction * kinematic_factor * (coupling_constants_ratio**2)
 
 
+def calculate_cross_sections_ratio_at_phi_peak(
+        parameters: Union[KaonParametersPhiRatio, KaonParametersPhiRatioSimple],
+        charged_kaon_mass: float,
+        neutral_kaon_mass: float,
+        alpha: float,
+        hc_squared: float,
+        use_radiative_correction: bool = True,
+) -> float:
+    parameters = parameters.copy()
+    mass_phi = parameters['mass_phi'].value
+    if use_radiative_correction:
+        radiative_correction = calculate_cremmer_gourdin_factor(mass_phi**2, alpha, charged_kaon_mass)
+    else:
+        radiative_correction = 1
+        parameters.fix_all_parameters()
+    f = make_partial_ff_or_cs_for_parameters(
+            alpha, hc_squared, parameters, charged_kaon_mass, neutral_kaon_mass
+    )
+    phi_mass = parameters['mass_phi'].value
+    ts = [
+        KaonDatapoint(t=phi_mass**2, is_charged=True, is_for_cross_section=True),
+        KaonDatapoint(t=phi_mass**2, is_charged=False, is_for_cross_section=True),
+    ]
+    res = f(ts)
+
+    return radiative_correction * res[0] / res[1]
+
+
 def calculate_r_ratio_with_error(
-        parameters: KaonParametersPhiRatio,
+        parameters: Union[KaonParametersPhiRatio, KaonParametersPhiRatioSimple],
         charged_kaon_mass: float,
         neutral_kaon_mass: float,
         alpha: float,
@@ -96,19 +125,57 @@ def read_error_from_report(report_filepath):
 
     return get_errors(data)
 
+
 if __name__ == '__main__':
     config = ConfigParser(inline_comment_prefixes='#')
     config.read('../configuration.ini')
     alpha = config.getfloat('constants', 'alpha')
+    hc_squared = config.getfloat('constants', 'hc_squared')
     charged_kaon_mass = config.getfloat('constants', 'charged_kaon_mass')
     neutral_kaon_mass = config.getfloat('constants', 'neutral_kaon_mass')
 
-    for i in [10, 25, 29, 33, 51, 54, 7, 67]:
+    for i in [33, 51, 74, 76]:
         kaon_parameters_filepath = f'/home/lukas/reports/kaons/runPhiRatio1_{i}/final_fit_parameters.pickle'
+        kaon_errors_filepath = f'/home/lukas/reports/kaons/runPhiRatio1_{i}/report_200.txt'
         kaon_parameters = KaonParametersPhiRatio.load_from_serialized_parameters(kaon_parameters_filepath)
+        mass_phi_error, a_phi_charged_error, a_phi_neutral_error = read_error_from_report(kaon_errors_filepath)
+
+        cs_ratio = calculate_cross_sections_ratio_at_phi_peak(
+            kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha, hc_squared, False)
 
         r_ratio_no_rc = calculate_r_ratio(
             kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha, use_radiative_correction=False)
+        r_ratio_no_rc2, err_no_rc = calculate_r_ratio_with_error(
+            kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha,
+            mass_phi_error, a_phi_charged_error, a_phi_neutral_error, use_radiative_correction=False)
+        assert r_ratio_no_rc == r_ratio_no_rc2
         r_ratio = calculate_r_ratio(
             kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha, use_radiative_correction=True)
-        print(f'Parameters runPhiRatio1_{i}: R ratio (no RC): {r_ratio_no_rc}; R ratio: {r_ratio}')
+        r_ratio2, err = calculate_r_ratio_with_error(
+            kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha,
+            mass_phi_error, a_phi_charged_error, a_phi_neutral_error, use_radiative_correction=True)
+        assert r_ratio == r_ratio2
+        print(f'Parameters runPhiRatio1_{i}: cs ratio (no RC): {cs_ratio}  R ratio (no RC): {r_ratio_no_rc}, {err_no_rc} ; R ratio: {r_ratio}, {err}')
+
+    for i in [13, 32, 41]:
+        kaon_parameters_filepath = f'/home/lukas/reports/kaons/runPhiRatio2_{i}/final_fit_parameters.pickle'
+        kaon_errors_filepath = f'/home/lukas/reports/kaons/runPhiRatio2_{i}/report_200.txt'
+        kaon_parameters = KaonParametersPhiRatioSimple.load_from_serialized_parameters(kaon_parameters_filepath)
+        mass_phi_error, a_phi_charged_error, a_phi_neutral_error = read_error_from_report(kaon_errors_filepath)
+
+        cs_ratio = calculate_cross_sections_ratio_at_phi_peak(
+            kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha, hc_squared, False)
+
+        r_ratio_no_rc = calculate_r_ratio(
+            kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha, use_radiative_correction=False)
+        r_ratio_no_rc2, err_no_rc = calculate_r_ratio_with_error(
+            kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha,
+            mass_phi_error, a_phi_charged_error, a_phi_neutral_error, use_radiative_correction=False)
+        assert r_ratio_no_rc == r_ratio_no_rc2
+        r_ratio = calculate_r_ratio(
+            kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha, use_radiative_correction=True)
+        r_ratio2, err = calculate_r_ratio_with_error(
+            kaon_parameters, charged_kaon_mass, neutral_kaon_mass, alpha,
+            mass_phi_error, a_phi_charged_error, a_phi_neutral_error, use_radiative_correction=True)
+        assert r_ratio == r_ratio2
+        print(f'Parameters runPhiRatio2_{i}: cs ratio (no RC): {cs_ratio} R ratio (no RC): {r_ratio_no_rc}, {err_no_rc} ; R ratio: {r_ratio}, {err}')
