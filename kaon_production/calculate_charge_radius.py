d665aa2e2de0cf22ed58746348d5a2c82a23a773
Lukas Holka <lukas.holka@savba.sk>
2024-04-11 11:09:26 +0200

    Update the charge radius script

--- kaon_production/calculate_charge_radius.py
+++ kaon_production/calculate_charge_radius.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 from typing import Callable
 from common.utils import make_partial_form_factor_for_parameters
-from model_parameters import KaonParametersFixedSelected, Parameter, PionParameters
+from model_parameters import KaonParametersSimplified, Parameter, PionParameters
 from kaon_production.data import KaonDatapoint
 from ua_model.PionUAModel import PionUAModel
 
@@ -14,8 +14,6 @@ def _wrap_partial_form_factor_function(partial_f: Callable, charged: bool = True
     def wrapped(s):
         datapoint = KaonDatapoint(t=s, is_charged=charged, is_for_cross_section=False)
         res = partial_f([datapoint])[0].real
-        label = 'charged' if charged else 'neutral'
-        print(f'ff_kaon_{label}({s})={res}')
         return res
     return wrapped
 
@@ -26,7 +24,6 @@ def _make_partial_for_pion_parameters(pion_parameters: PionParameters) -> Callab
 
     def wrapped(s):
         res = ff_model(s).real
-        print(f'ff_pion({s})={res}')
         return res
     return wrapped
 
@@ -43,27 +40,43 @@ if __name__ == '__main__':
     # to obtain the result in fm^2 we need to multiply by 10^-7,
     # because 1nb = 10^-37m^2 and 1fm^2 = 10^-30m^2
     hc_squared = hc_squared * 1e-7
+    print(f'hc^2: {hc_squared}')
 
-    # kaon_parameters_filepath = '/home/lukas/reports/kaons/run2/run2_6/final_fit_parameters.pickle'
-    # kaon_parameters_filepath = '/home/lukas/reports/kaons/run3/run3_0/final_fit_parameters.pickle'
-    # kaon_parameters_filepath = '/home/lukas/reports/kaons/run4/run4_0/final_fit_parameters.pickle'
-    # kaon_parameters_filepath = '/home/lukas/reports/kaons/run4/run4_29/final_fit_parameters.pickle'
-    # kaon_parameters_filepath = '/home/lukas/reports/kaons/run5_56/final_fit_parameters.pickle'
-    # kaon_parameters = KaonParametersFixedSelected.load_from_serialized_parameters(kaon_parameters_filepath)
-    # kaon_parameters.fix_all_parameters()
-    # f = _wrap_partial_form_factor_function(
-    #     make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #     charged=True
-    # )
-    # charge_radius_charged = calculate_charge_radius(f, hc_squared)
-    # print(f'Charge radius charged kaon: {charge_radius_charged}')
-    #
-    # f = _wrap_partial_form_factor_function(
-    #     make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #     charged=False
-    # )
-    # charge_radius_neutral = calculate_charge_radius(f, hc_squared)
-    # print(f'Charge radius neutral kaon: {charge_radius_neutral}')
+    for i in [2, 29, 40, 125, 151]:
+        kaon_parameters_filepath = f'/home/lukas/reports/kaons/run9simplified_{i}/final_fit_parameters.pickle'
+        kaon_parameters = KaonParametersSimplified.load_from_serialized_parameters(kaon_parameters_filepath)
+        kaon_parameters.fix_all_parameters()
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=True
+        )
+        charge_radius_charged = calculate_charge_radius(f, hc_squared)
+        print(f'{kaon_parameters_filepath} Charge radius charged kaon: {charge_radius_charged}')
+
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=False
+        )
+        charge_radius_neutral = calculate_charge_radius(f, hc_squared)
+        print(f'{kaon_parameters_filepath} Charge radius neutral kaon: {charge_radius_neutral}')
+
+    for i in [37, 113, 160, 165]:
+        kaon_parameters_filepath = f'/home/lukas/reports/kaons/runTestDressedsimplified_{i}/final_fit_parameters.pickle'
+        kaon_parameters = KaonParametersSimplified.load_from_serialized_parameters(kaon_parameters_filepath)
+        kaon_parameters.fix_all_parameters()
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=True
+        )
+        charge_radius_charged = calculate_charge_radius(f, hc_squared)
+        print(f'{kaon_parameters_filepath} Charge radius charged kaon: {charge_radius_charged}')
+
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=False
+        )
+        charge_radius_neutral = calculate_charge_radius(f, hc_squared)
+        print(f'{kaon_parameters_filepath} Charge radius neutral kaon: {charge_radius_neutral}')
 
     pion_parameters = PionParameters.from_list([
         Parameter(name='t_0_isovector', value=0.07791957505900839, is_fixed=True),
@@ -86,13 +99,3 @@ if __name__ == '__main__':
     pion_charge_radius = calculate_charge_radius(f_pion, hc_squared)
     print(f'Charge radius charged pion: {pion_charge_radius}')
 
-    test_pion_parameters = pion_parameters.copy()
-    ff_model_pions = PionUAModel(**{p.name: p.value for p in pion_parameters})
-    def f(s):
-        res = ff_model_pions(s)
-        print(f'ff_pion({s})={res}')
-        return res
-
-    test_values = [0.0, 0.05, 0.1, 0.5, 1.0, 1.2, 1.5, 2.0]
-    for s in test_values:
-        f(s)
