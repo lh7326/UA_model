37f264e3b1640f4943dcadc2518d471b5a719510
Lukas Holka <lukas.holka@savba.sk>
2024-04-17 13:13:44 +0200

    Continue implementing HLbL calculations

--- kaon_production/calculate_hlbl_contribution.py
+++ kaon_production/calculate_hlbl_contribution.py
@@ -75,7 +75,7 @@ def _calculate_pi_n(form_factor_function, q1squared, q2squared, q3squared, n, pa
     integral = dblquad(
         lambda y, x: _calculate_i_n(x, y, q1squared, q2squared, q3squared, n, particle_mass),
         a=0, b=1, gfun=0, hfun=lambda x: 1-x,
-    )
+    )[0]
     return (
         form_factor_function(q1squared) * form_factor_function(q2squared) *
         form_factor_function(q3squared) * integral / (16 * (math.pi**2))
@@ -86,7 +86,7 @@ def calculate_pi_n(form_factor_function, q1squared, q2squared, q3squared, n, par
     if n in {1, 4, 7, 17, 39, 54}:
         return _calculate_pi_n(form_factor_function, q1squared, q2squared, q3squared, n, particle_mass)
     elif n == 2:
-        return _calculate_pi_n(form_factor_function, q1squared, q3squared, q2squared, 2, particle_mass)
+        return _calculate_pi_n(form_factor_function, q1squared, q3squared, q2squared, 1, particle_mass)
     elif n == 5:
         return _calculate_pi_n(form_factor_function, q1squared, q3squared, q2squared, 4, particle_mass)
     elif n == 9:
@@ -348,9 +348,11 @@ def calculate_t_n(q1squared, q2squared, q3squared, n, muon_mass):
         )
         f = 4.0 * q2 * q1squared * q1squared * q1 * capital_x
         return (a + b + c + d + e + f) / (4.0 * muon_mass_squared * q1 * q2 * q3squared)
+    else:
+        raise ValueError(f'Unsupported value of n: {n}')
 
 
-def _calculate_a_hlbl_contribution(form_factor_function, alpha, particle_mass, muon_mass):
+def calculate_hlbl_contribution(form_factor_function, alpha, particle_mass, muon_mass):
     def integrand(phi, r, sigma):
         q1squared, q2squared, q3squared = _transform_variables(sigma, r, phi)
         acc = 0
@@ -360,70 +362,49 @@ def _calculate_a_hlbl_contribution(form_factor_function, alpha, particle_mass, m
             acc += kernel * pi
         return acc
     return (alpha**3 / (432 * (math.pi**2)) ) * tplquad(
-        integrand, a=0, b=numpy.inf, gfun=0, hfun=1, qfun=0, rfun=2*math.pi)
+        integrand, a=0, b=numpy.inf, gfun=0, hfun=1, qfun=0, rfun=2*math.pi)[0]
 
 
 if __name__ == '__main__':
 
-    # config = ConfigParser(inline_comment_prefixes='#')
-    # config.read('../configuration.ini')
-    # alpha = config.getfloat('constants', 'alpha')
-    # charged_kaon_mass = config.getfloat('constants', 'charged_kaon_mass')
-    #
-    # for i in [2, 29, 40, 125, 151]:
-    #     kaon_parameters_filepath = f'/home/lukas/reports/kaons/run9simplified_{i}/final_fit_parameters.pickle'
-    #     kaon_parameters = KaonParametersSimplified.load_from_serialized_parameters(kaon_parameters_filepath)
-    #     kaon_parameters.fix_all_parameters()
-    #     f = _wrap_partial_form_factor_function(
-    #         make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #         charged=True
-    #     )
-    #     em_mass2_charged_kaon = calculate_em_mass2_contribution(f, alpha, charged_kaon_mass)
-    #     print(f'{kaon_parameters_filepath} EM mass^2 charged kaon: {em_mass2_charged_kaon}')
-    #
-    #     f = _wrap_partial_form_factor_function(
-    #         make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #         charged=False
-    #     )
-    #     em_mass2_neutral_kaon = calculate_em_mass2_contribution(f, alpha, charged_kaon_mass)
-    #     print(f'{kaon_parameters_filepath} EM mass^2 neutral kaon: {em_mass2_neutral_kaon}')
-    #
-    # for i in [37, 113, 160, 165]:
-    #     kaon_parameters_filepath = f'/home/lukas/reports/kaons/runTestDressedsimplified_{i}/final_fit_parameters.pickle'
-    #     kaon_parameters = KaonParametersSimplified.load_from_serialized_parameters(kaon_parameters_filepath)
-    #     kaon_parameters.fix_all_parameters()
-    #     f = _wrap_partial_form_factor_function(
-    #         make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #         charged=True
-    #     )
-    #     em_mass2_charged_kaon = calculate_em_mass2_contribution(f, alpha, charged_kaon_mass)
-    #     print(f'{kaon_parameters_filepath} EM mass^2 charged kaon: {em_mass2_charged_kaon}')
-    #
-    #     f = _wrap_partial_form_factor_function(
-    #         make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #         charged=False
-    #     )
-    #     em_mass2_neutral_kaon = calculate_em_mass2_contribution(f, alpha, charged_kaon_mass)
-    #     print(f'{kaon_parameters_filepath} EM mass^2 neutral kaon: {em_mass2_neutral_kaon}')
-    #
-    # pion_parameters = PionParameters.from_list([
-    #     Parameter(name='t_0_isovector', value=0.07791957505900839, is_fixed=True),
-    #     Parameter(name='t_in_isovector', value=1.2733, is_fixed=False),
-    #     Parameter(name='mass_rho', value=0.7621, is_fixed=False),
-    #     Parameter(name='decay_rate_rho', value=0.14423672, is_fixed=False),
-    #     Parameter(name='a_rho_prime', value=-0.07060638, is_fixed=False),
-    #     Parameter(name='mass_rho_prime', value=1.3500, is_fixed=False),
-    #     Parameter(name='decay_rate_rho_prime', value=0.3319913, is_fixed=False),
-    #     Parameter(name='a_rho_double_prime', value=0.05785514, is_fixed=False),
-    #     Parameter(name='mass_rho_double_prime', value=1.76928672, is_fixed=False),
-    #     Parameter(name='decay_rate_rho_double_prime', value=0.25311443, is_fixed=False),
-    #     Parameter(name='a_rho_triple_prime', value=0.00208887, is_fixed=False),
-    #     Parameter(name='mass_rho_triple_prime', value=2.24674832, is_fixed=False),
-    #     Parameter(name='decay_rate_rho_triple_prime', value=0.0700, is_fixed=False),
-    #     Parameter(name='w_pole', value=0.38329263, is_fixed=False),
-    #     Parameter(name='w_zero', value=0.2844582, is_fixed=False),
-    # ])
-    # f_pion = _make_partial_for_pion_parameters(pion_parameters)
-    # pion_em_m2 = calculate_em_mass2_contribution(f_pion, alpha, charged_kaon_mass)
-    # print(f'EM mass^2 charged pion: {pion_em_m2}')
-    #
+    config = ConfigParser(inline_comment_prefixes='#')
+    config.read('../configuration.ini')
+    alpha = config.getfloat('constants', 'alpha')
+    charged_kaon_mass = config.getfloat('constants', 'charged_kaon_mass')
+    muon_mass = config.getfloat('constants', 'muon_mass')
+
+    for i in [2, 29, 40, 125, 151]:
+        kaon_parameters_filepath = f'/home/lukas/reports/kaons/run9simplified_{i}/final_fit_parameters.pickle'
+        kaon_parameters = KaonParametersSimplified.load_from_serialized_parameters(kaon_parameters_filepath)
+        kaon_parameters.fix_all_parameters()
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=True
+        )
+        em_mass2_charged_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
+        print(f'{kaon_parameters_filepath} HLbL contribution charged kaon: {em_mass2_charged_kaon}')
+
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=False
+        )
+        em_mass2_neutral_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
+        print(f'{kaon_parameters_filepath} HLbL neutral kaon: {em_mass2_neutral_kaon}')
+
+    for i in [37, 113, 160, 165]:
+        kaon_parameters_filepath = f'/home/lukas/reports/kaons/runTestDressedsimplified_{i}/final_fit_parameters.pickle'
+        kaon_parameters = KaonParametersSimplified.load_from_serialized_parameters(kaon_parameters_filepath)
+        kaon_parameters.fix_all_parameters()
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=True
+        )
+        em_mass2_charged_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
+        print(f'{kaon_parameters_filepath} HLbL contribution charged kaon: {em_mass2_charged_kaon}')
+
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=False
+        )
+        em_mass2_neutral_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
+        print(f'{kaon_parameters_filepath} HLbL contribution neutral kaon: {em_mass2_neutral_kaon}')
