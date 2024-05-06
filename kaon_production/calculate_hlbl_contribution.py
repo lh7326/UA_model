a35d326a3a98f2a6b89871200f32e113497e72e9
Lukas Holka <lukas.holka@savba.sk>
2024-05-06 09:17:05 +0200

    Calculate with a greater precision

--- kaon_production/calculate_hlbl_contribution.py
+++ kaon_production/calculate_hlbl_contribution.py
@@ -18,8 +18,8 @@ import math
 import numpy
 from scipy.integrate import dblquad, tplquad
 
-REL_ERROR_INNER_INTEGRAL = 1.49e-1
-REL_ERROR_OUTER_INTEGRAL = 1.49e-1
+REL_ERROR_INNER_INTEGRAL = 1.49e-5
+REL_ERROR_OUTER_INTEGRAL = 1.49e-2
 
 
 def _wrap_partial_form_factor_function(partial_f: Callable, charged: bool = True) -> Callable:
@@ -529,27 +529,27 @@ if __name__ == '__main__':
         em_mass2_charged_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
         print(f'{kaon_parameters_filepath} HLbL contribution charged kaon: {em_mass2_charged_kaon}')
 
-    #     f = _wrap_partial_form_factor_function(
-    #         make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #         charged=False
-    #     )
-    #     em_mass2_neutral_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
-    #     print(f'{kaon_parameters_filepath} HLbL neutral kaon: {em_mass2_neutral_kaon}')
-    #
-    # for i in [37, 113, 160, 165]:
-    #     kaon_parameters_filepath = f'/home/lukas/reports/kaons/runTestDressedsimplified_{i}/final_fit_parameters.pickle'
-    #     kaon_parameters = KaonParametersSimplified.load_from_serialized_parameters(kaon_parameters_filepath)
-    #     kaon_parameters.fix_all_parameters()
-    #     f = _wrap_partial_form_factor_function(
-    #         make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #         charged=True
-    #     )
-    #     em_mass2_charged_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
-    #     print(f'{kaon_parameters_filepath} HLbL contribution charged kaon: {em_mass2_charged_kaon}')
-    #
-    #     f = _wrap_partial_form_factor_function(
-    #         make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
-    #         charged=False
-    #     )
-    #     em_mass2_neutral_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
-    #     print(f'{kaon_parameters_filepath} HLbL contribution neutral kaon: {em_mass2_neutral_kaon}')
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=False
+        )
+        em_mass2_neutral_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
+        print(f'{kaon_parameters_filepath} HLbL neutral kaon: {em_mass2_neutral_kaon}')
+
+    for i in [37, 113, 160, 165]:
+        kaon_parameters_filepath = f'/home/lukas/reports/kaons/runTestDressedsimplified_{i}/final_fit_parameters.pickle'
+        kaon_parameters = KaonParametersSimplified.load_from_serialized_parameters(kaon_parameters_filepath)
+        kaon_parameters.fix_all_parameters()
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=True
+        )
+        em_mass2_charged_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
+        print(f'{kaon_parameters_filepath} HLbL contribution charged kaon: {em_mass2_charged_kaon}')
+
+        f = _wrap_partial_form_factor_function(
+            make_partial_form_factor_for_parameters(kaon_parameters, return_absolute_value=False),
+            charged=False
+        )
+        em_mass2_neutral_kaon = calculate_hlbl_contribution(f, alpha, charged_kaon_mass, muon_mass)
+        print(f'{kaon_parameters_filepath} HLbL contribution neutral kaon: {em_mass2_neutral_kaon}')
